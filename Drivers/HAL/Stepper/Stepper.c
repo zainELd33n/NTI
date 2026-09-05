4f448add01274ded86ea2cf7b46d3bf6488e9aa6
Zain~San <117827268+zainELd33n@users.noreply.github.com>
2026-09-05 19:42:37 +0300

    Added Stepper motor driver

---
+++ Drivers/HAL/Stepper/Stepper.c
@@ -0,0 +1,84 @@
+#define F_CPU 1000000UL
+#include "../../SERVICE/std_type.h"
+#include "../../MCAL/DIO/DIO_int.h"
+#include <util/delay.h>
+
+void Stepper_half_clkWise(u8 port) {
+	u8 half_step_sequence[8] = {
+		0b00000011,
+		0b00000010,
+		0b00000110,
+		0b00000100,
+		0b00001100,
+		0b00001000,
+		0b00001001,
+		0b00000001
+	};
+	
+	DIO_SetPortDirection(port, OUTPUT);
+	
+	for (u8 i = 0; i < 8; i++)
+	{
+		DIO_SetPortValue(port, half_step_sequence[i]);
+		_delay_ms(1000);
+	}
+	DIO_SetPortValue(port, 0b00000011);
+}
+
+void Stepper_half_anticlkWise(u8 port) {
+	u8 half_step_sequence[8] = {
+		0b00000011,
+		0b00000010,
+		0b00000110,
+		0b00000100,
+		0b00001100,
+		0b00001000,
+		0b00001001,
+		0b00000001
+	};
+	
+	DIO_SetPortDirection(port, OUTPUT);
+	
+	for (s8 i = 7; i >= 0; i--)
+	{
+		DIO_SetPortValue(port, half_step_sequence[i]);
+		_delay_ms(1000);
+	}
+	DIO_SetPortValue(port, 0b00000011);
+}
+
+void Stepper_full_clkWise(u8 port) {
+	u8 half_step_sequence[8] = {
+		0b00000011,
+		0b00000110,
+		0b00001100,
+		0b00001001,
+		0b00000011
+	};
+	
+	DIO_SetPortDirection(port, OUTPUT);
+	
+	for (u8 i = 0; i < 5; i++)
+	{
+		DIO_SetPortValue(port, half_step_sequence[i]);
+		_delay_ms(1000);
+	}
+}
+
+void Stepper_full_anticlkWise(u8 port) {
+	u8 half_step_sequence[8] = {
+		0b00000011,
+		0b00000110,
+		0b00001100,
+		0b00001001,
+		0b00000011
+	};
+	
+	DIO_SetPortDirection(port, OUTPUT);
+	
+	for (s8 i = 5; i >= 0; i--)
+	{
+		DIO_SetPortValue(port, half_step_sequence[i]);
+		_delay_ms(1000);
+	}
+}
---
+++ Drivers/HAL/Stepper/Stepper.h
@@ -0,0 +1,5 @@
+void Stepper_half_clkWise(u8 port);
+void Stepper_half_anticlkWise(u8 port);
+
+void Stepper_full_clkWise(u8 port);
+void Stepper_full_anticlkWise(u8 port);
